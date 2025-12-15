d7f11bf021d370c97fe9646ca1c0436b810c990f
matheeshapr <matheeshap62@gmail.com>
2025-12-15 13:25:22 +0530

    Enables AddBookService for book addition, adds UI feedback and clears fields.

--- src/main/java/controller/AddBookPageController.java
+++ src/main/java/controller/AddBookPageController.java
@@ -2,11 +2,17 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
-import model.entity.Books;
-import service.AddBookService;
+import service.impl.AddBookServiceimpl;
 
 public class AddBookPageController {
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtTitle;
 
     @FXML
     private TextField txtAuthor;
@@ -15,28 +21,29 @@ public class AddBookPageController {
     private TextField txtCategory;
 
     @FXML
-    private TextField txtId;
-
-    @FXML
     private TextField txtQnty;
 
-    @FXML
-    private TextField txtTitle;
-
-    private AddBookService addBookService = new AddBookService();
+    private AddBookServiceimpl addBookService = new AddBookServiceimpl();
 
     @FXML
     void btnAddBook(ActionEvent event) {
-
         String id = txtId.getText();
         String title = txtTitle.getText();
         String author = txtAuthor.getText();
         String category = txtCategory.getText();
         int qty = Integer.parseInt(txtQnty.getText());
 
-        Books books = new Books(id,title,author,category,qty);
+        addBookService.addBook(id, title, author, category, qty);
 
-        String isSaved = addBookService.saveBook(books);
+        new Alert(Alert.AlertType.INFORMATION, "Book Added Successfully!").show();
+        clearFields();
+    }
 
+    private void clearFields() {
+        txtId.setText("");
+        txtTitle.setText("");
+        txtAuthor.setText("");
+        txtCategory.setText("");
+        txtQnty.setText("");
     }
 }
