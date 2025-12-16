5a660d2f0c9763154064090845c06a79351824dd
matheeshapr <matheeshap62@gmail.com>
2025-12-16 19:45:57 +0530

    Implements full login form with username, password, and login button.

--- src/main/java/controller/LoginPageController.java
+++ src/main/java/controller/LoginPageController.java
@@ -1,4 +1,50 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginPageController {
+
+    @FXML
+    private Button btnlogin;
+
+    @FXML
+    private PasswordField txtpWord;
+
+    @FXML
+    private TextField txtuName;
+
+    @FXML
+    void onbtnlogin(ActionEvent event) {
+        String uname = txtuName.getText();
+        String pword = txtpWord.getText();
+
+        //if (uname.equals("admin") && pword.equals(("1234"))){
+            navigateDashboard();
+//        }
+//        else {
+//            new Alert(Alert.AlertType.ERROR,"Invalidate Username or Password").show();
+//        }
+
+    }
+
+    private void navigateDashboard() {
+        Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/DashboardPage.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+    }
+
 }
