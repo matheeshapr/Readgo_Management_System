3ea06fe4f78c5d7b01733f753fdf75ab274c9b8b
matheeshapr <matheeshap62@gmail.com>
2025-12-16 19:44:33 +0530

    Opens the Add Book form from the dashboard.

--- src/main/java/controller/DashboardPageController.java
+++ src/main/java/controller/DashboardPageController.java
@@ -1,4 +1,20 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashboardPageController {
+    public void btbonaddbook(ActionEvent actionEvent) {
+        Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/AddBook.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+    }
 }
