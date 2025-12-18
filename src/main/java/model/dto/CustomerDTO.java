be0492d2a45c81dff162b063ab6fd45f4daabd28
matheeshapr <matheeshap62@gmail.com>
2025-12-18 16:26:47 +0530

    Sets up CustomerDTO with customer details fields and Lombok annotations.

--- src/main/java/model/dto/CustomerDTO.java
+++ src/main/java/model/dto/CustomerDTO.java
@@ -1,4 +1,17 @@
 package model.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class CustomerDTO {
+
+    private String id;
+    private String name;
+    private String contact;
+    private String email;
+
 }
