a2ff21d2bccb1e069b4acbac244d80d3f68b5fcc
matheeshapr <matheeshap62@gmail.com>
2025-12-20 22:15:46 +0530

    Adds RentDTO fields to display customer view.

--- src/main/java/model/dto/RentDTO.java
+++ src/main/java/model/dto/RentDTO.java
@@ -1,4 +1,11 @@
 package model.dto;
 
+import java.util.Date;
+
 public class RentDTO {
+    private String name;
+    private String id;
+    private String description;
+    private Date date;
+
 }
