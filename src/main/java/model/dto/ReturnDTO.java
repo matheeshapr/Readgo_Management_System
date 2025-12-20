66279a21cb2707159fb357992c73037de0503bfe
matheeshapr <matheeshap62@gmail.com>
2025-12-20 22:16:40 +0530

    Adds customer rent view fields to ReturnDTO.

--- src/main/java/model/dto/ReturnDTO.java
+++ src/main/java/model/dto/ReturnDTO.java
@@ -1,4 +1,11 @@
 package model.dto;
 
+import java.util.Date;
+
 public class ReturnDTO {
+
+    private String name;
+    private String id;
+    private String description;
+    private Date date;
 }
