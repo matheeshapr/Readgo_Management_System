20909da8b91500acc410e2e1205e950bed2de792
matheeshapr <matheeshap62@gmail.com>
2025-12-21 09:34:03 +0530

    Renames AddBookRepository to BookRepository.

--- src/main/java/repository/BookRepository.java
+++ src/main/java/repository/BookRepository.java
@@ -1,7 +1,5 @@
 package repository;
 
-import model.dto.BookDTO;
-
-public interface AddBookRepository {
+public interface BookRepository {
     void addBook(String id, String title, String author, String category, int qty);
 }
