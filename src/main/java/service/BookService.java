a31f11e1674fdcf02fa817f8ce800623bf35d64a
matheeshapr <matheeshap62@gmail.com>
2025-12-21 09:35:03 +0530

    Renames AddBookService to BookService to reflect broader scope.

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -1,7 +1,5 @@
 package service;
 
-import model.dto.BookDTO;
-
-public interface AddBookService {
+public interface BookService {
     void addBook(String id, String title, String author, String category, int qty);
 }
