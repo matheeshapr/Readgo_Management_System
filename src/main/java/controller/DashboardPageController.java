1858e8da4c438febb78b2648aa09dde54057fe85
matheeshapr <matheeshap62@gmail.com>
2025-12-26 09:31:08 +0530

    Enables JavaFX dashboard navigation for rent, return, report, and logout.

--- src/main/java/controller/DashboardPageController.java
+++ src/main/java/controller/DashboardPageController.java
@@ -30,11 +30,46 @@ public class DashboardPageController {
     }
 
     public void btnonrent(ActionEvent actionEvent) {
+        Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/RentPage.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
     }
 
     public void btnonreturn(ActionEvent actionEvent) {
+        Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/ReturnPage.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
     }
 
     public void btnonreport(ActionEvent actionEvent) {
+        Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/ReportPage.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+    }
+
+    public void btnOnLogout(ActionEvent actionEvent) {
+        Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/LoginPage.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+
+    }
+
+    public void btnonsettings(ActionEvent actionEvent) {
     }
 }
