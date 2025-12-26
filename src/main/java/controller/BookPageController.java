895f7846445a0b35d760f58cde8f9ba0d6c3d410
matheeshapr <matheeshap62@gmail.com>
2025-12-26 09:30:31 +0530

    Adds BookPage button handlers, enables dashboard navigation, updates JavaFX.

--- src/main/java/controller/BookPageController.java
+++ src/main/java/controller/BookPageController.java
@@ -2,9 +2,14 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import service.impl.BookServiceimpl;
+
+import java.io.IOException;
 
 public class BookPageController {
 
@@ -46,4 +51,25 @@ public class BookPageController {
         txtCategory.setText("");
         txtQnty.setText("");
     }
+
+
+    public void btnUpdate(ActionEvent actionEvent) {
+    }
+
+    public void btnDelete(ActionEvent actionEvent) {
+    }
+
+    public void btnClear(ActionEvent actionEvent) {
+    }
+
+    public void btnonback(ActionEvent actionEvent) {
+        Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/DashboardPage.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.show();
+    }
+
 }
