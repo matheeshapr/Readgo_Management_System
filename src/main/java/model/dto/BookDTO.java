465acd9658be53def1749e4d263f916370acd191
matheeshapr <matheeshap62@gmail.com>
2025-12-27 12:37:44 +0530

    Adds explicit BookDTO fields for ReturnBookRepository and RentBookService.

--- src/main/java/model/dto/BookDTO.java
+++ src/main/java/model/dto/BookDTO.java
@@ -2,15 +2,91 @@ package model.dto;
 
 import lombok.*;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class BookDTO {
     private String id;
     private String title;
     private String author;
     private String category;
+    private double price;
     private int qty;
+    private String status;
+
+    public BookDTO(String id, String title, String author, String category, double price, int qty, String status) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.price = price;
+        this.qty = qty;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "BookDTO{" +
+                "id='" + id + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                ", qty=" + qty +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
