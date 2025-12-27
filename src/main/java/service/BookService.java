cfb8ff94d13ca0128898e8e60aa72bb27e7de8cf
matheeshapr <matheeshap62@gmail.com>
2025-12-27 12:39:36 +0530

    Implements book CRUD and adds price and status DTO fields.

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -1,5 +1,18 @@
 package service;
 
+import javafx.collections.ObservableList;
+import model.dto.BookDTO;
+
 public interface BookService {
-    void addBook(String id, String title, String author, String category, int qty);
+
+    void addBook(String id, String title, String author, String category, double price, int qty, String status);
+
+    void deleteBook(String id);
+
+    void updateBook(String id, String title, String author, String category, double price, int qty, String status);
+
+    ObservableList<BookDTO> getAllBooks();
+
+    BookDTO searchBook(String id);
+
 }
