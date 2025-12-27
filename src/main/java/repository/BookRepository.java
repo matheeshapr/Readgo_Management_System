37a07984cfdc55101a5d4ee113f37bf9bdd9da7c
matheeshapr <matheeshap62@gmail.com>
2025-12-27 12:38:58 +0530

    Implements book CRUD and adds book DTO fields.

--- src/main/java/repository/BookRepository.java
+++ src/main/java/repository/BookRepository.java
@@ -1,5 +1,17 @@
 package repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public interface BookRepository {
-    void addBook(String id, String title, String author, String category, int qty);
+
+    void addBook(String id, String title, String author, String category, double price, int qty, String status);
+
+    void deleteBook(String id);
+
+    void updateBook(String id, String title, String author, String category, double price, int qty, String status);
+
+    ResultSet getAllBooks() throws SQLException;
+
+    ResultSet searchBook(String id) throws SQLException;
 }
