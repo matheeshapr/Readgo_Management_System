153e079cbe729aa9925ec701833fac2b71a47185
matheeshapr <matheeshap62@gmail.com>
2025-12-28 09:08:02 +0530

    Enhances CustomerRepository interface with methods for updating, deleting, retrieving, and searching customers

--- src/main/java/repository/CustomerRepository.java
+++ src/main/java/repository/CustomerRepository.java
@@ -1,5 +1,16 @@
 package repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public interface CustomerRepository {
     void addCustomer(String id, String name, String contact, String email);
+
+    void updateCustomer(String id, String name, String contact, String email);
+
+    void deleteCustomer(String id);
+
+    ResultSet getAllCustomers() throws SQLException;
+
+    ResultSet searchCustomer(String id) throws SQLException;
 }
