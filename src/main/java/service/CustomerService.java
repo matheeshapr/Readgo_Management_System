e5244784de87ca623aade39d0ce93010571ad68c
matheeshapr <matheeshap62@gmail.com>
2025-12-28 09:08:43 +0530

    Adds methods for customer management in CustomerService interface, including add, update, delete, and retrieval of customers.

--- src/main/java/service/CustomerService.java
+++ src/main/java/service/CustomerService.java
@@ -1,4 +1,14 @@
 package service;
 
+import javafx.collections.ObservableList;
+import model.dto.CustomerDTO;
+
 public interface CustomerService {
+    void addCustomer(String id, String name, String contact, String email);
+
+    void updateCustomer(String id, String name, String contact, String email);
+
+    void deleteCustomer(String id);
+
+    ObservableList<CustomerDTO> getAllCustomers();
 }
