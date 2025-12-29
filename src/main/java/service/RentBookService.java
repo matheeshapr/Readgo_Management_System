9134d85df702fbeaf5df30c7c50ade93a444a3fb
matheeshapr <matheeshap62@gmail.com>
2025-12-29 09:43:21 +0530

    Adds methods for managing rents in RentBookService, including placing, deleting, updating, and retrieving all rents.

--- src/main/java/service/RentBookService.java
+++ src/main/java/service/RentBookService.java
@@ -1,4 +1,12 @@
 package service;
 
+import javafx.collections.ObservableList;
+import model.dto.RentDTO;
+import java.time.LocalDate;
+
 public interface RentBookService {
+    void placeRent(String rentId, String bookId, String custId, LocalDate issueDate, LocalDate dueDate, String status);
+    void deleteRent(String rentId);
+    void updateRent(String rentId, String bookId, String custId, LocalDate issueDate, LocalDate dueDate, String status);
+    ObservableList<RentDTO> getAllRents();
 }
