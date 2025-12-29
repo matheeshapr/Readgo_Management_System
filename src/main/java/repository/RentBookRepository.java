37e95b2e2d0ba66aa455ce65fa7041313a8a2949
matheeshapr <matheeshap62@gmail.com>
2025-12-29 09:43:00 +0530

    Adds methods for managing rent records in RentBookRepository, including add, delete, update, and retrieval of all rents.

--- src/main/java/repository/RentBookRepository.java
+++ src/main/java/repository/RentBookRepository.java
@@ -1,4 +1,12 @@
 package repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+
 public interface RentBookRepository {
+    void addRent(String rentId, String bookId, String custId, LocalDate issueDate, LocalDate dueDate, String status);
+    void deleteRent(String rentId);
+    void updateRent(String rentId, String bookId, String custId, LocalDate issueDate, LocalDate dueDate, String status);
+    ResultSet getAllRents() throws SQLException;
 }
