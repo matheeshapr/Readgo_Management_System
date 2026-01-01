36b9e7ea7979bf33fc5845c1309a1cecb5db63fd
matheeshapr <matheeshap62@gmail.com>
2026-01-01 08:10:25 +0530

    Refactors ReturnDTO to include rentId, bookId, returnDate, lateDays, and fine attributes

--- src/main/java/model/dto/ReturnDTO.java
+++ src/main/java/model/dto/ReturnDTO.java
@@ -1,17 +1,25 @@
 package model.dto;
 
-import lombok.*;
+import java.time.LocalDate;
 
-import java.util.Date;
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 public class ReturnDTO {
+    private String rentId;
+    private String bookId;
+    private LocalDate returnDate;
+    private int lateDays;
+    private double fine;
 
-    private String name;
-    private String id;
-    private String description;
-    private Date date;
+    public ReturnDTO(String rentId, String bookId, LocalDate returnDate, int lateDays, double fine) {
+        this.rentId = rentId;
+        this.bookId = bookId;
+        this.returnDate = returnDate;
+        this.lateDays = lateDays;
+        this.fine = fine;
+    }
+
+    public String getRentId() { return rentId; }
+    public String getBookId() { return bookId; }
+    public LocalDate getReturnDate() { return returnDate; }
+    public int getLateDays() { return lateDays; }
+    public double getFine() { return fine; }
 }
